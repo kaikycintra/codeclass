9c50aca18a1ccb7334b33d41e23c9204684802ed
kaiky <kaikycintra@gmail.com>
2025-05-16 12:15:10 -0300

    feat: adiciona página de login

--- servidor/ccapp/urls.py
+++ servidor/ccapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "ccapp"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
 ]
--- servidor/ccapp/views.py
+++ servidor/ccapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
-
+@require_http_methods(["GET"])
 def index(request):
     return render(request, "ccapp/landing.html")
+
+@require_http_methods(["GET", "POST"])
+def login(request):
+    if request.method == "POST":
+        pass
+    return render(request, "ccapp/login.html")
